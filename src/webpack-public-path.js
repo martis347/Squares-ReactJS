44f3ed54877138227cc0c74a19dc2ec2dc94dd4f
Martynas Kanapinskas <martis347@gmail.com>
2017-02-02 20:23:45 +0200

    Initial commit with solution template

---
+++ src/webpack-public-path.js
@@ -0,0 +1,3 @@
+// Dynamically set the webpack public path at runtime below
+// eslint-disable-next-line no-undef
+__webpack_public_path__ = window.location.protocol + "//" + window.location.host + "/";
---
+++ tools/setup/setupMessage.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-console */
+console.log('===========================');
+console.log('=  Squares ReactJS Setup  =');
+console.log('===========================\n');
+console.log('Installing dependencies. Please wait...');
